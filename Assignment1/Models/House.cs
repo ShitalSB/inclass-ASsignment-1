be89aab309fa3c0b7cf0eb210c344c5eabadf620
Shital Bhutiya <ssbhutiya@gmail.com>
2018-09-21 09:37:36 -0500

    added house, car and person classes

---
+++ Assignment1/Models/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class House
+    {
+        // Added properties of House
+        public int Value { get; set; }
+        public float LotSize { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
